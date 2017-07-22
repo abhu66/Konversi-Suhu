f9a46346453a0b54ae16526136608b320df153dc
Abu Khoerul Iskandar Ali <khoirulabhu@gmail.com>
2017-07-22 17:38:01 +0700

    KONVERSI SUHU

---
+++ src/MAIN/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MAIN;
+
+import VIEW.FormSuhu;
+
+/**
+ *
+ * @author Abu Khoerul I A
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                FormSuhu dialog = new FormSuhu(new javax.swing.JFrame(), true);
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+    
+}
